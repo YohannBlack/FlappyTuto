c394465c17402e002d746273f3fba82fd2e22d5d
Yotheblack <blackburnyohann@gmail.com>
2021-01-25 22:45:37 +0100

    TODO : Test collision ligne + cercle

---
+++ src/Model/Avancer.java
@@ -0,0 +1,31 @@
+package Model;
+
+public class Avancer extends Thread{
+
+    /*** Attributs ***/
+    private Parcours parcours;
+    private Etat etat;
+
+    /*** Constantes ***/
+    private final int DELAY = 100;
+
+    public Avancer (Parcours parcours, Etat etat){
+        this.parcours = parcours;
+        this.etat = etat;
+    }
+
+    /**
+     * Permet de faire avancer la ligne
+     */
+    public void run(){
+        while(!etat.testPerdu()){
+            System.out.println(!etat.testPerdu());
+            parcours.setPosition();
+            try {
+                Thread.sleep(DELAY);
+            } catch (InterruptedException e) { e.printStackTrace(); }
+        }
+
+    }
+
+}
